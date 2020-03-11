65a1c71a436a479f41aeba17e904cd3aa0021eca
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-03-11 08:01:52 +0000

    complit project

--- src/components/DisplayNotes.js
+++ src/components/DisplayNotes.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { Button, Card } from 'semantic-ui-react'
+import { Button, Card, Form, TextArea } from 'semantic-ui-react'
 import moment from 'moment';
 
 
 class DisplayNotes extends Component {
 
-    state = {}
+    state = {
+        isEdit: false,
+    }
 
 
     handleClick = (noteId) => {
         this.props.removeNote(noteId);
+    }
+
+    handleEdit = () => {
+        this.setState({
+            isEdit: !this.state.isEdit,
+        })
     }
 
 
@@ -24,13 +32,19 @@ class DisplayNotes extends Component {
                                 <Card.Meta>
                                     <span className='date'>{moment(note.createdAt).format("DD/MM/YYYY")}</span>
                                 </Card.Meta>
-                                <Card.Description>
-                                    {note.note}
-                                </Card.Description>
+                                {this.state.isEdit ? (
+                                    <Form>
+                                        <TextArea value={note.note} />
+                                    </Form>
+                                ) : (
+                                        <Card.Description>
+                                            {note.note}
+                                        </Card.Description>
+                                    )}
                             </Card.Content>
                             <Card.Content extra>
                                 <div className='ui two buttons'>
-                                    <Button basic color='green'>
+                                    <Button basic color='green' onClick={this.handleEdit}>
                                         Edit
                                     </Button>
                                     <Button basic color='red' onClick={() => this.handleClick(note.id)}>
